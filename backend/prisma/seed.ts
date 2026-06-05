027322006a8fe29fc54a5e97cee5bb32594eb22d
ravindu senanayake <123373439+ravndusenanayake@users.noreply.github.com>
2026-06-05 10:05:26 +0530

    feat: initialize backend project with Express, Prisma, and database seeding scripts

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,13 +1,8 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
-
 import bcrypt from 'bcryptjs';
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 const DUMMY_PRODUCTS = [
   {
